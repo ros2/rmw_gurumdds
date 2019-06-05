d2e6eb814b192b5fef50a0af67f02b73c4e7d5b8
junho <junho@gurum.cc>
2019-06-05 14:28:05 +0900

    migration from gitlab

---
+++ README.md
@@ -0,0 +1,31 @@
+# rmw_coredds
+Implementation of the ROS middleware interface using [GurumNetworks CoreDDS](http://www.gurum.cc).
+
+## Requirements
+This project requires `rosidl_typesupport_coredds` to be built. For more information, see README.md of the [project](https://github.com/gurumnet/rosidl_typesupport_coredds).
+
+## Packages
+This project consists of two packages, `rmw_coredds_cpp`, `rmw_coredds_shared_cpp` and `demo_nodes_cpp_native_coredds`.  
+
+### rmw_coredds_cpp
+`rmw_coredds_cpp` is the rmw implementation. You can use this rmw implementation by setting environment variable `RMW_IMPLEMENTATION=rmw_coredds_cpp` after installation. For `rmw_coredds_cpp` to work properly, make sure to set environment variable `COREDDS_CONFIG=$COREDDS_HOME/coredds.yaml` and set `allow_loopback` variable in `coredds.yaml` to `true`. If you are not familiar with [YAML](https://yaml.org/), please note that YAML only supports spaces, not tabs, for indentation.  
+
+```
+DATA:
+  allow_loopback: true
+  dynamic_buffer: true
+  mtu: auto # auto | number(1472)
+  bitmap_capacity: 256
+  buffer_capacity: 512
+```
+
+### rmw_coredds_shared_cpp
+`rmw_coredds_shared_cpp` contains some functions used by `rmw_coredds_cpp`. `rmw_coredds_dynamic_cpp` is not implemented yet, but `rmw_coredds_shared_cpp` is separated for expandability.  
+
+### demo_nodes_cpp_native_coredds
+`demo_nodes_cpp_natvie_coredds` is similar to `demo_nodes_cpp_native` from ROS2 `demos`. This demo shows how to access the native handles of `rmw_coredds_cpp`.
+
+## Implementation Status
+Currently some features are not fully implemented.
+- DDS Security
+- Entity status
---
+++ rmw_coredds_shared_cpp/src/qos.cpp
@@ -0,0 +1,161 @@
+// Copyright 2019 GurumNetworks, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+#include "rmw_coredds_shared_cpp/qos.hpp"
+
+bool is_time_default(const rmw_time_t & time)
+{
+  return time.sec == 0 && time.nsec == 0;
+}
+
+dds_Duration_t
+rmw_time_to_dds(const rmw_time_t & time)
+{
+  dds_Duration_t duration;
+  duration.sec = static_cast<int32_t>(time.sec);
+  duration.nanosec = static_cast<uint32_t>(time.nsec);
+  return duration;
+}
+
+template<typename dds_EntityQos>
+bool
+set_entity_qos_from_profile_generic(
+  const rmw_qos_profile_t * qos_profile,
+  dds_EntityQos * entity_qos)
+{
+  switch (qos_profile->history) {
+    case RMW_QOS_POLICY_HISTORY_KEEP_LAST:
+      entity_qos->history.kind = dds_KEEP_LAST_HISTORY_QOS;
+      break;
+    case RMW_QOS_POLICY_HISTORY_KEEP_ALL:
+      entity_qos->history.kind = dds_KEEP_ALL_HISTORY_QOS;
+      break;
+    case RMW_QOS_POLICY_HISTORY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("unknown qos history policy");
+      return false;
+  }
+
+  switch (qos_profile->reliability) {
+    case RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT:
+      entity_qos->reliability.kind = dds_BEST_EFFORT_RELIABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_RELIABILITY_RELIABLE:
+      entity_qos->reliability.kind = dds_RELIABLE_RELIABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("unknown qos reliability policy");
+      return false;
+  }
+
+  switch (qos_profile->durability) {
+    case RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL:
+      entity_qos->durability.kind = dds_TRANSIENT_LOCAL_DURABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_DURABILITY_VOLATILE:
+      entity_qos->durability.kind = dds_VOLATILE_DURABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_DURABILITY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("unknown qos durability policy");
+      return false;
+  }
+
+  if (qos_profile->depth != RMW_QOS_POLICY_DEPTH_SYSTEM_DEFAULT) {
+    entity_qos->history.depth = static_cast<int32_t>(qos_profile->depth);
+  }
+
+  if (!is_time_default(qos_profile->deadline)) {
+    entity_qos->deadline.period = rmw_time_to_dds(qos_profile->deadline);
+  }
+
+  switch (qos_profile->liveliness) {
+    case RMW_QOS_POLICY_LIVELINESS_AUTOMATIC:
+      entity_qos->liveliness.kind = dds_AUTOMATIC_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE:
+      entity_qos->liveliness.kind = dds_MANUAL_BY_PARTICIPANT_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC:
+      entity_qos->liveliness.kind = dds_MANUAL_BY_TOPIC_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("unknown qos liveliness policy");
+      return false;
+  }
+
+  if (!is_time_default(qos_profile->liveliness_lease_duration)) {
+    entity_qos->liveliness.lease_duration = rmw_time_to_dds(qos_profile->liveliness_lease_duration);
+  }
+
+  // ensure the history depth is at least the requested queue size
+  assert(entity_qos->history.depth >= 0 || entity_qos->history.depth == dds_LENGTH_UNLIMITED);
+  if (
+    entity_qos->history.kind == dds_KEEP_LAST_HISTORY_QOS &&
+    static_cast<size_t>(entity_qos->history.depth) < qos_profile->depth)
+  {
+    if (qos_profile->depth > (std::numeric_limits<int32_t>::max)()) {
+      RMW_SET_ERROR_MSG(
+        "failed to set history depth since the requested queue size exceeds the DDS type");
+      return false;
+    }
+    entity_qos->history.depth = static_cast<int32_t>(qos_profile->depth);
+  }
+
+  return true;
+}
+
+bool
+get_datawriter_qos(
+  dds_Publisher * publisher,
+  const rmw_qos_profile_t * qos_profile,
+  dds_DataWriterQos * datawriter_qos)
+{
+  dds_ReturnCode_t ret = dds_Publisher_get_default_datawriter_qos(publisher, datawriter_qos);
+  if (ret != dds_RETCODE_OK) {
+    RMW_SET_ERROR_MSG("failed to get default datawriter qos");
+    return false;
+  }
+
+  if (!is_time_default(qos_profile->lifespan)) {
+    datawriter_qos->lifespan.duration = rmw_time_to_dds(qos_profile->lifespan);
+  }
+
+  set_entity_qos_from_profile_generic(qos_profile, datawriter_qos);
+
+  return true;
+}
+
+bool get_datareader_qos(
+  dds_Subscriber * subscriber,
+  const rmw_qos_profile_t * qos_profile,
+  dds_DataReaderQos * datareader_qos)
+{
+  dds_ReturnCode_t ret = dds_Subscriber_get_default_datareader_qos(subscriber, datareader_qos);
+  if (ret != dds_RETCODE_OK) {
+    RMW_SET_ERROR_MSG("failed to get default datareader qos");
+    return false;
+  }
+
+  set_entity_qos_from_profile_generic(qos_profile, datareader_qos);
+
+  return true;
+}
