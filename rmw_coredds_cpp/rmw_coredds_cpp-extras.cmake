b7e09d65894773eba83622b891456f4521b281d6
junho <junho@gurum.cc>
2019-06-11 17:31:45 +0900

    updated cmake to fit new library paths

--- rmw_coredds_cpp/rmw_coredds_cpp-extras.cmake
+++ rmw_coredds_cpp/rmw_coredds_cpp-extras.cmake
@@ -15,5 +15,5 @@
 find_package(coredds_cmake_module REQUIRED)
 find_package(CoreDDS REQUIRED MODULE)
 
-list(APPEND rmw_coredds_cpp_INCLUDE_DIRS ${CoreDDS_INCLUDE_DIRS})
-list(APPEND rmw_coredds_cpp_LIBRARIES ${CoreDDS_LIBRARIES})
+list(APPEND rmw_coredds_cpp_INCLUDE_DIRS ${CoreDDS_INCLUDE_DIR})
+list(APPEND rmw_coredds_cpp_LIBRARIES ${CoreDDS_LIBRARY})
