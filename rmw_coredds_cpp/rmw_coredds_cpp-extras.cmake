f28980751859ac26f30e5641bfa2437e1bcf950e
junho <junho@gurum.cc>
2019-06-13 14:41:47 +0900

    fixed typos

--- rmw_coredds_cpp/rmw_coredds_cpp-extras.cmake
+++ rmw_coredds_cpp/rmw_coredds_cpp-extras.cmake
@@ -16,4 +16,4 @@ find_package(coredds_cmake_module REQUIRED)
 find_package(CoreDDS REQUIRED MODULE)
 
 list(APPEND rmw_coredds_cpp_INCLUDE_DIRS ${CoreDDS_INCLUDE_DIR})
-list(APPEND rmw_coredds_cpp_LIBRARIES ${CoreDDS_LIBRARY})
+list(APPEND rmw_coredds_cpp_LIBRARIES ${CoreDDS_LIBRARIES})
