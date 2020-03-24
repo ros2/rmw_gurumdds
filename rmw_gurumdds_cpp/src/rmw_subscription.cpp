7f61248bf9f805166b20e42a5aa00b64c3eb4e3e
junho <junho@gurum.cc>
2020-03-24 17:08:03 +0900

    Added qos finalization after creating publisher/subscriber

--- rmw_gurumdds_cpp/src/rmw_subscription.cpp
+++ rmw_gurumdds_cpp/src/rmw_subscription.cpp
@@ -155,6 +155,13 @@ rmw_create_subscription(
     dds_DomainParticipant_create_subscriber(participant, &subscriber_qos, nullptr, 0);
   if (dds_subscriber == nullptr) {
     RMW_SET_ERROR_MSG("failed to create subscriber");
+    dds_SubscriberQos_finalize(&subscriber_qos);
+    goto fail;
+  }
+
+  ret = dds_SubscriberQos_finalize(&subscriber_qos);
+  if (ret != dds_RETCODE_OK) {
+    RMW_SET_ERROR_MSG("failed to finalize subscriber qos");
     goto fail;
   }
 
