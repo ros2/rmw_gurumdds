5eeb3b2bd4d9bcfd0d77aafba45eee0209e9a4d2
junho <junho@gurum.cc>
2020-06-08 13:55:53 +0900

    Fixed demo build error

--- demo_nodes_cpp_native_gurumdds/src/talker.cpp
+++ demo_nodes_cpp_native_gurumdds/src/talker.cpp
@@ -48,7 +48,7 @@ public:
     timer_ = create_wall_timer(500ms, publish);
     pub_ = create_publisher<std_msgs::msg::String>("chatter", 10);
 
-    rcl_publisher_t * rcl_pub = pub_->get_publisher_handle();
+    rcl_publisher_t * rcl_pub = (pub_->get_publisher_handle()).get();
     rmw_publisher_t * rmw_pub = rcl_publisher_get_rmw_handle(rcl_pub);
     dds_Publisher * dds_pub = rmw_gurumdds_cpp::get_publisher(rmw_pub);
     dds_DataWriter * dds_dw = rmw_gurumdds_cpp::get_data_writer(rmw_pub);
