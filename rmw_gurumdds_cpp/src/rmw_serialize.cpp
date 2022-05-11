8fd2aeffc175b6bdde20bf536cc91fb3f257f7f8
Youngjin Yun <youngjin@gurum.cc>
2022-05-11 13:38:40 +0900

    Fix rclcpp test(test_serialized_message) failure

--- rmw_gurumdds_cpp/src/rmw_serialize.cpp
+++ rmw_gurumdds_cpp/src/rmw_serialize.cpp
@@ -57,6 +57,7 @@ rmw_serialize(
       serialized_message->allocator.allocate(
         serialized_message->buffer_length,
         serialized_message->allocator.state));
+    serialized_message->buffer_capacity = size;
   }
 
   bool res = serialize_ros_to_cdr(
