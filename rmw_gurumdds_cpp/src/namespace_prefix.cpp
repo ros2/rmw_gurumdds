75083c474c213ff62ad161dc3dbd7f3421d2fe97
Youngjin Yun <youngjin@gurum.cc>
2022-07-19 16:11:08 +0900

    Avoid string generation on func call

--- rmw_gurumdds_cpp/src/namespace_prefix.cpp
+++ rmw_gurumdds_cpp/src/namespace_prefix.cpp
@@ -21,7 +21,7 @@ const char * const ros_topic_prefix = "rt";
 const char * const ros_service_requester_prefix = "rq";
 const char * const ros_service_response_prefix = "rr";
 
-std::vector<std::string> _ros_prefixes =
+const std::vector<std::string> _ros_prefixes =
 {ros_topic_prefix, ros_service_requester_prefix, ros_service_response_prefix};
 
 std::string _get_ros_prefix_if_exists(const std::string & topic_name)
